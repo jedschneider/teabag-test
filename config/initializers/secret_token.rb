05d544cf7e506925d36f49cac503068a7f3ca567
Jed Schneider <jed.schneider@modeset.com>
2013-04-24 11:16:28 -0400

    inital import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeabagTest::Application.config.secret_token = '8d3eac5dafc86e1e164469bd93b5d77ff64d374e79279cdf3d314a921d78ddf6d6db58f5aaf1c8cad4998fe823088d1ee4e51a0191588ca5259dc3ac8c054859'
